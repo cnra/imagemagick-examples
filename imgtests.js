0479e00f576022d98dcc1ce3cd03debb5bdb6693
rrr <roser137@hotmail.com>
2020-09-21 18:53:26 +0300

    express-async-errors

--- imgtests.js
+++ imgtests.js
@@ -1,27 +1,41 @@
-const express = require('express');
-var im = require('imagemagick');
+const express = require("express");
+const imagemagick = require("imagemagick");
 
-const util = require('util');
-const fs = require('fs');
+const util = require("util");
+const fs = require("fs");
+const identify = util.promisify(imagemagick.identify);
 
 const port = process.env["port"] || 8080;
 const app = express();
 
-app.use('/images', express.static('images'))
+require('express-async-errors');
 
-app.get("/", async (req, res) => {
-  var images = fs.readdirSync('./images');
-  var imagelinks = images.map(i => `<a style='border: 1px solid #000; padding: 3px' href="info/${i}">info</a> <a href="images/${i}">${i}</a>`);
+app.use("/images", express.static("images"));
+
+app.get("/", async (req, res, next) => {
+  throw new Error("mmm vvv");
+  var images = fs.readdirSync("./images");
+  var imagelinks = images.map(
+    (i) =>
+      `<a style='border: 1px solid #000; padding: 3px' href="info/${i}">info</a> 
+       <a href="images/${i}">${i}</a>`
+  );
   res.end(imagelinks.join("<br />"));
-})
+});
 
 app.get("/info/:fn", async (req, res) => {
   let fn = req.params["fn"];
 
-  im.identify(`./images/${fn}`, function (err, info) {
-    if (err) res.end(err.message);
-    res.json(info);
-  })
+  let info = await identify(`./images/${fn}`);
+
+  delete info["channel statistics"];
+  delete info["image statistics"];
+  delete info["artifacts"];
+  if (info["properties"]) delete info["properties"]["date:create"];
+  if (info["properties"]) delete info["properties"]["date:modify"];
+  if (info["properties"]) delete info["properties"]["date:delete"];
+  if (info["properties"]) delete info["properties"]["signature"];
+  res.json(info);
 });
 
 app.listen(port);
