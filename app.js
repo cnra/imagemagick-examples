576467a6876b194f4449fbb2d5fb9d511cac61e3
rrr <roser137@hotmail.com>
2020-09-21 15:02:30 +0300

    launch json

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const ExifImage = require('exif').ExifImage;
 
+const fs = require('fs');
+
 const port = 8080;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.end("hi")
+app.use('/images', express.static('images'))
+
+app.get("/", async (req, res) => {
+  var images = fs.readdirSync('./images');
+  var imagelinks = images.map(i => `<a href="images/${i}">${i}</a><br>`);
+  res.end(imagelinks.join());
 })
 
+async function test() {
+  var images = await fs.readdirSync('./images');
+  console.log(images);
+}
+
+test();
 
 app.listen(port);
 console.log(`listening on ${port}`);
